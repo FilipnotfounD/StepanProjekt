37e71750532d75243139c81d56b887823bf9c4ca
1107 <1107@student.itgymnazium.cz>
2024-05-06 23:02:01 +0200

    second

---
+++ source/repos/StepanProjekt/StepanProjekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StepanProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
